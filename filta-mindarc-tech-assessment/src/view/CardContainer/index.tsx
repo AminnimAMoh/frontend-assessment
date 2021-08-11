d796ba5301e2b3a782fff3d06e1aabed1f35ce1d
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-08-11 17:44:05 +1000

    Cards done!
    Chacking for details.

--- filta-mindarc-tech-assessment/src/view/CardContainer/index.tsx
+++ filta-mindarc-tech-assessment/src/view/CardContainer/index.tsx
@@ -26,24 +26,24 @@ export default function CardContainer({}: Props): ReactElement {
   ];
   return (
     <div className="container cards mb-5">
-      <div className="row justify-content-around">
+      <div className="row justify-content-around pt-5">
         {cardData.map(({ text, margin }) => {
           return (
-              <div className="card col-lg-4 col-md-12 mt-md-5 box-shadow text-center p-4 cards__card">
-                <img
-                  className="card-img-top"
-                  src="http://via.placeholder.com/400x300"
-                  alt="Card_Content"
-                />
-                <div className="card-body d-flex flex-column cards__body">
-                  <p className="card-text mt-3 mb-4 cards__body-pragraph">
-                    {text}
-                  </p>
-                  <div className="mt-auto btn btn-lg btn-block text-uppercase cards__body-button">
-                    Read more
-                  </div>
+            <div className="card col-lg-4 col-md-12 box-shadow text-center p-2 cards__card">
+              <img
+                className="card-img-top"
+                src="http://via.placeholder.com/400x300"
+                alt="Card_Content"
+              />
+              <div className="card-body d-flex flex-column cards__body">
+                <p className="card-text mt-3 mb-4 cards__body-pragraph">
+                  {text}
+                </p>
+                <div className="mt-auto btn btn-lg btn-block text-uppercase cards__body-button">
+                  Read more
                 </div>
               </div>
+            </div>
           );
         })}
       </div>
